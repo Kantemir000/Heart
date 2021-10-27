3a1d523cfbee95eb2eb1788fea0de250b8a576fb
Kantemir Kuzminov <kantik1406@gmail.com>
2021-10-27 17:48:39 +0300

    Website optimization, working with gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,26 @@
 /* Использование пакетов (var заменили на const для оптимизации) */
 const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass')(require('sass'));
 const rename = require("gulp-rename");
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+
 
 
 gulp.task('server', function() {  /* task - задача */ /* 'название' функция() */
 
     browserSync({ 
         server: {
-            baseDir: "src" /* наш live server будет запускаться из папки src */
+            baseDir: "dist" /* src *//* наш live server будет запускаться из папки src */
         },
         browser: 'google chrome', /* safari */
         open: 'local'
     });
+    gulp.watch("src/*.html").on('change'/*  изменение файла */, browserSync.reload);
+    gulp.watch("src/js/script.js").on('change'/*  изменение файла */, browserSync.reload);
 });
 
 gulp.task('styles', function() {
@@ -27,14 +32,39 @@ gulp.task('styles', function() {
         .pipe(autoprefixer({
 			cascade: false
 		}))
-        .pipe(gulp.dest("src/css")) /* файлы, которые получились положили по (...) адресу */
+        .pipe(gulp.dest("dist/css")) /* src */ /* файлы, которые получились положили по (...) адресу */
         .pipe(browserSync.stream()); /* запускать браузер */
 });
 
-gulp.task('watch', function() { /* чтобы gulp следил за изменениями html и стилестических файлов */
-    gulp.watch("src/sass/**/*.+(scss|sass)"/*отследит изменения*/ , gulp.parallel('styles') /* после gulp запустит style*/)
-    gulp.watch("src/*.html").on('change'/*  изменение файла */, browserSync.reload);
-    gulp.watch("src/js/script.js").on('change'/*  изменение файла */, browserSync.reload);
-})
+gulp.task('watch', function() { /* чтобы gulp следил за изменениями */
+    gulp.watch("src/sass/**/*.+(scss|sass|css)"/*отследит изменения*/ , gulp.parallel('styles') /* после gulp запустит style*/);
+    gulp.watch("src/*.html").on('change', gulp.parallel('html')); /* когда этот файл будет изменятся будет запускаться задача с названием html */
+});
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles')); /* одной коммандой запустить весь функционал */
+gulp.task('html', function() {
+    return gulp.src("src/*.html") /* получаем файл над которым работаем  */
+        .pipe(htmlmin({ collapseWhitespace: true })) /* убераются все пробелы в файле */
+        .pipe(gulp.dest("dist/")); /* помещаем в папку dist */
+});
+
+gulp.task('js', function() {
+    return gulp.src("src/js/**/*.js")
+        .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('fonts', function() {
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task('mailer', function() {
+    return gulp.src("src/mailer/**/*")
+        .pipe(gulp.dest("dist/mailer"));
+});
+
+gulp.task('img', function() {
+    return gulp.src("src/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img"));
+});
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'js', 'fonts', 'mailer', 'img')); /* одной коммандой запустить весь функционал */
